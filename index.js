d0ab9e4f2ff8450d64acf0cf139517aa432d20f4
rajendramalappanavar <59605361+rajendramalappanavar@users.noreply.github.com>
2021-03-04 23:11:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const axios = require('axios')
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+//
+//listen for request
+app.listen(3000)
+//
+app.get('/', async (req, res) => {
+
+  const results = await axios('https://disease.sh/v3/covid-19/all')
+  console.log(results.data)
+
+
+  res.render('index', {
+    totalCases : results.data.cases,
+    recovered: results.data.recovered,
+    todayCases : results.data.todayCases,
+    todayRecovered: results.data.todayRecovered,
+    totalDeaths : results.data.deaths,
+    todayDeaths : results.data.todayDeaths,
+    active : results.data.active,
+    critical : results.data.critical
+  });
+});
+//
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+//
+app.get('/contact', (req,res) => {
+  res.render('contact');
+});
+//
+app.use((req, res) => {
+  res.status(404).render('404');
+});
